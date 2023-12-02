18e38a11d01957a6ec5bec5e494677e2fcdedc77
Vasster <vasilhspinhs@gmail.com>
2023-12-02 14:05:03 +0200

    Added Attack Function for player1
    The player 1 when pressed the attack button goes to the player 2 position with a stoppining distance and attacks

--- TurnBasedLocalPvp/Turn Based Combat Local PVP/Assets/PVPManager.cs
+++ TurnBasedLocalPvp/Turn Based Combat Local PVP/Assets/PVPManager.cs
@@ -11,8 +11,8 @@ public class PVPManager : MonoBehaviour
     private IEnumerator coroutineStartGame;
     private IEnumerator coroutinePlayer1;
     private IEnumerator coroutinePlayer2;
-    public Animator animPlayer1;
-    public Animator animPlayer2;
+    [SerializeField] private float runSpeed = 5;
+    [SerializeField] public bool hasPressedAttack = false;
     private void Start()
     {
         Cameras[0].SetActive(true);
@@ -39,16 +39,43 @@ public class PVPManager : MonoBehaviour
         playerUI[0].SetActive(true);
         playerUI[1].SetActive(false);
     }
-    public void Attack()
+    public void AttackButtonPlayer1()
     {
         playerUI[0].SetActive(false);
         playerUI[1].SetActive(false);
         Cameras[0].SetActive(true);
         Cameras[1].SetActive(false);
         Cameras[2].SetActive(false);
-    //    animPlayer1.SetTrigger("AttackP1");
-        coroutinePlayer1=player1Coroutine(2.0f);
+        //   animPlayer1.SetTrigger("AttackP1");
+        coroutinePlayer1 = player1Coroutine(8.0f);
         StartCoroutine(coroutinePlayer1);
+        hasPressedAttack = true;
+    }
+    public void MoveTowardsPlayerAndAttack()
+    {
+        if (hasPressedAttack)
+        {
+            Animator playerAnimator = players[0].GetComponent<Animator>();
+            playerAnimator.SetBool("isRunning", true);
+
+            Vector3 player2Position = players[1].transform.position;
+            float distanceToTarget = Vector3.Distance(players[0].transform.position, player2Position);
+            Debug.Log(distanceToTarget);
+         players[0].transform.position = Vector3.MoveTowards(players[0].transform.position, player2Position, Time.deltaTime * runSpeed);
+            
+             if(distanceToTarget<2f)
+            {
+                players[0].transform.LookAt(players[1]);
+                playerAnimator.SetTrigger("Attack");
+                hasPressedAttack = false;
+                Debug.Log("Attacked");          
+            }
+        }
+    }
+
+    private void Update()
+    {
+        MoveTowardsPlayerAndAttack();
     }
     private IEnumerator player1Coroutine(float waitTime)
     {
