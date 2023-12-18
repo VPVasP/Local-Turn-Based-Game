b165da9bb3295cff6cb451d7cdd24294b523d852
Vasster <vasilhspinhs@gmail.com>
2023-12-18 14:05:30 +0200

    Added Comments and cleaned up Code

--- TurnBasedLocalPvp/Turn Based Combat Local PVP/Assets/Scripts/PVPManager.cs
+++ TurnBasedLocalPvp/Turn Based Combat Local PVP/Assets/Scripts/PVPManager.cs
@@ -3,56 +3,61 @@ using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-using UnityEngine.UIElements;
-
 public class PVPManager : MonoBehaviour
 {
     public static PVPManager instance;
-    public GameObject[] Cameras;
-    public GameObject[] playerUI;
-    public Transform[] players;
-    public Transform[] startingPositions;
+    public GameObject[] Cameras; //our cameras refrence
+    public GameObject[] playerUI; //our player ui gameobject refrence
+    public Transform[] players; //our players transform
+    public Transform[] startingPositions; //the startingposition transforms
+    //coroutines refrences
     private IEnumerator coroutineStartGame;
     private IEnumerator coroutinePlayer1;
     private IEnumerator coroutinePlayer2;
     private IEnumerator coroutinePlayer1returntoStartPos;
     private IEnumerator coroutineHealPlayer1;
     private IEnumerator coroutineHealPlayer2;
-    [SerializeField] private float runSpeed = 5;
-    [SerializeField] public bool hasPressedAttack = false;
-    [SerializeField] public bool hasPressedAttackP2 = false;
-    public bool player1Attacking = false;
-    public bool player2Attacking = false;
-    public GameObject[] healths;
-    public AudioClip[] audioClipsAttackPlayer1;
-    public AudioClip audioClipHurtPlayer1;
-    public AudioClip audioClipHurtPlayer2;
-    public AudioClip[] audioClipsAttackPlayer2;
+
+    [SerializeField] private float runSpeed = 5; //the runspeed for both players
+    //bools to handle attack
+    [SerializeField] private bool hasPressedAttack = false;
+    [SerializeField] private bool hasPressedAttackP2 = false;
+    private bool player1Attacking = false;
+    private bool player2Attacking = false;
+    public GameObject[] healths; //health game objects array
+    //audioclips and audiosources variables
+   [SerializeField] private AudioClip[] audioClipsAttackPlayer1;
+   [SerializeField] private AudioClip audioClipHurtPlayer1;
+    [SerializeField] private AudioClip audioClipHurtPlayer2;
+    [SerializeField]private AudioClip[] audioClipsAttackPlayer2;
     [SerializeField] private AudioClip healAudioClip;
-    public AudioSource aud;
-    public AudioSource mainMusic;
-    public GameObject winnerScreen;
-    public GameObject[] healthPotions;
+    private AudioSource aud;
+    [SerializeField ] private AudioSource mainMusic;
+    public GameObject winnerScreen; //winner screen gameobject
+    public GameObject[] healthPotions; //health potion game object array
     private void Awake()
     {
         instance = this;
     }
     private void Start()
     {
+        //we set the correct cameras to true and others to false through the array
         Cameras[0].SetActive(true);
         Cameras[1].SetActive(false);
         Cameras[2].SetActive(false);
         Cameras[3].SetActive(false);
         Cameras[4].SetActive(false);
+        //we set the playerui to false at the start
         playerUI[0].SetActive(false);
         playerUI[1].SetActive(false);
-        coroutineStartGame = startGameCoroutine(3.0f);
+        coroutineStartGame = startGameCoroutine(3.0f); //we call the coroutine 
         StartCoroutine(coroutineStartGame);
         aud = GetComponent<AudioSource>();
         winnerScreen.SetActive(false);
         healthPotions[0].SetActive(false);
         healthPotions[1].SetActive(false);
     }
+    //coroutine for delaying the start of the game
     private IEnumerator startGameCoroutine(float waitTime)
     {
       
@@ -61,6 +66,7 @@ public class PVPManager : MonoBehaviour
         print("Coroutine for startGameCoroutine Ended " + Time.time + " seconds");
     }
     #region player1Stuff
+    //method that activates player 1 ui and the player 1 camera
     public void Player1Turn()
     {
         Cameras[0].SetActive(false);
@@ -72,8 +78,10 @@ public class PVPManager : MonoBehaviour
         playerUI[1].SetActive(false);
         players[1].transform.LookAt(startingPositions[0].transform);
     }
+    //method for handling player1 attack
     public void AttackButtonPlayer1()
     {
+       // activates player 1 ui and the player 1 camera to attack
         playerUI[0].SetActive(false);
         playerUI[1].SetActive(false);
         Cameras[0].SetActive(true);
@@ -81,49 +89,63 @@ public class PVPManager : MonoBehaviour
         Cameras[2].SetActive(false);
         Cameras[3].SetActive(false);
         Cameras[4].SetActive(false);
+        //plays a random attack sound from the array
         int randomIndex = Random.Range(0, audioClipsAttackPlayer1.Length);
         mainMusic.volume = 0.3f;
         aud.clip = audioClipsAttackPlayer1[randomIndex];
         aud.Play();
-        //   animPlayer1.SetTrigger("AttackP1");
         coroutinePlayer1 = player1Coroutine(8.0f);
-        StartCoroutine(coroutinePlayer1);
+        StartCoroutine(coroutinePlayer1); 
         hasPressedAttack = true;
     }
+    // method for moving player 1 towards player 2 and attacking
     public void MoveTowardsPlayerAndAttackPlayer1()
     {
         if (hasPressedAttack)
         {
+            //we set the target position and make player 1 look at it
             Transform lookatPosition = startingPositions[1].transform;
             Vector3 targetPositionlookat = lookatPosition.position;
             players[0].transform.LookAt(lookatPosition);
+            //we get the Animator component of player 1
             Animator playerAnimator = players[0].GetComponent<Animator>();
             playerAnimator.SetBool("isRunning", true);;
+            //get the position of player 2
             Vector3 player2Position = players[1].transform.position;
+            //we calculate the distance to the player 2
             float distanceToTarget = Vector3.Distance(players[0].transform.position, player2Position);
           Debug.Log(distanceToTarget);
+            //move player 1 towards player 2
          players[0].transform.position = Vector3.MoveTowards(players[0].transform.position, player2Position, Time.deltaTime * runSpeed);
-            healths[0].gameObject.SetActive(false);
+            healths[0].gameObject.SetActive(false); //disable player 1 health when he is going towards player 2
+
+            //checcks if the distance bettween player 1 and 2 is less than a value
              if(distanceToTarget<1.4)
             {
+                //we look at player 2 and we trigger the attack animation
                 players[0].transform.LookAt(players[1]);
                 playerAnimator.SetTrigger("Attack");
                 playerAnimator.SetBool("isRunning", false);
                 hasPressedAttack = false;
                 //Debug.Log("Attacked");
+                //we get the animator of player2 and play the hurt animation
                 Animator playerAnimator2 = players[1].GetComponent<Animator>();
                 playerAnimator2.SetTrigger("isHurt");
+                //we play the hurt audio of player2
                 aud.clip = audioClipHurtPlayer2;
                 aud.Play();
                 player1Attacking = true;
+                //begin the coroutine to return player 1 to the starting position
                 coroutinePlayer1returntoStartPos = player1CoroutineReturnToStartPos(1.0f);
                 StartCoroutine(coroutinePlayer1returntoStartPos);
 
             }
         }
     }
+    //heal player1
     public void HealPlayer1()
     {
+        //we handle the ui and cameras 
         playerUI[0].SetActive(false);
         playerUI[1].SetActive(false);
         Cameras[0].SetActive(false);
@@ -131,12 +153,15 @@ public class PVPManager : MonoBehaviour
         Cameras[2].SetActive(false);
         Cameras[3].SetActive(true);
         Cameras[4].SetActive(false);
+        //we get the player 1 animator 
         Animator playerAnimator = players[0].GetComponent<Animator>();
         playerAnimator.SetTrigger("Heal");
+        //we call the gain health function of player1
         players[0].GetComponent<PlayerHealth>().GainHealth();
         mainMusic.volume = 0.3f;
         coroutinePlayer1 = player1Coroutine(8.0f);
         Invoke("PlayHealSound", 3f);
+        //we set the health potion at array of 0 to true and 1 to false just in case
         healthPotions[0].SetActive(true);
         healthPotions[1].SetActive(false);
         StartCoroutine(coroutinePlayer1);
@@ -149,25 +174,27 @@ public class PVPManager : MonoBehaviour
     }
     private IEnumerator player1Coroutine(float waitTime)
     {
+        //deactivate health potions for both players
         yield return new WaitForSeconds(waitTime);
         player1Attacking = false;
         mainMusic.volume = 1f;
         healthPotions[0].SetActive(false);
         healthPotions[1].SetActive(false);
         StartCoroutine(coroutinePlayer1);
-        Player2Turn(); 
+        Player2Turn(); //transition to player 2 turn
         print("Coroutine for player1 Ended " + Time.time + " seconds");
     }
     private IEnumerator player1CoroutineReturnToStartPos(float waitTime)
     {
         yield return new WaitForSeconds(waitTime);
-        Transform startingPositionLookat = startingPositions[1].transform;
-        Vector3 targetPositionlookat = startingPositionLookat.position;
+        Transform startingPositionLookat = startingPositions[1].transform;//get the transform of the look-at position for player 1
+        Vector3 targetPositionlookat = startingPositionLookat.position;//Get the target position for looking at the opponent
         mainMusic.volume = 1f;
-        Transform startingPosition = startingPositions[0].transform;
+        Transform startingPosition = startingPositions[0].transform; //set the starting position transform 
         Vector3 targetPosition = startingPosition.position;
 
         float elapsedTime = 0f;
+        //get player 1 animator and play the animation
         Animator playerAnimator = players[0].GetComponent<Animator>();
         playerAnimator.SetBool("isRunning", true);
        
@@ -179,10 +206,11 @@ public class PVPManager : MonoBehaviour
             yield return null;
         }
 
-       
+    
         players[0].transform.position = targetPosition;
         playerAnimator.SetBool("isRunning", false);
         healths[0].gameObject.SetActive(true);
+        //look at the target position during the movement
         players[0].transform.LookAt(targetPositionlookat);
         print("Coroutine for player1 return to start position Ended " + Time.time + " seconds");
     }
@@ -259,7 +287,6 @@ public class PVPManager : MonoBehaviour
             playerAnimator.SetBool("isRunning", true); ;
             Vector3 player2Position = players[0].transform.position;
             float distanceToTarget = Vector3.Distance(players[1].transform.position, player2Position);
-         //   Debug.Log(distanceToTarget);
             players[1].transform.position = Vector3.MoveTowards(players[1].transform.position, player2Position, Time.deltaTime * runSpeed);
             healths[1].gameObject.SetActive(false);
             if (distanceToTarget < 1.6f)
@@ -268,7 +295,6 @@ public class PVPManager : MonoBehaviour
                 playerAnimator.SetTrigger("Attack");
                 playerAnimator.SetBool("isRunning", false);
                 hasPressedAttackP2 = false;
-            //    Debug.Log("Attacked");
                 Animator playerAnimator2 = players[0].GetComponent<Animator>();
                 playerAnimator2.SetTrigger("isHurt");
                 aud.clip = audioClipHurtPlayer2;
@@ -290,8 +316,6 @@ public class PVPManager : MonoBehaviour
         float elapsedTime = 0f;
         Animator playerAnimator = players[1].GetComponent<Animator>();
         playerAnimator.SetBool("isRunning",true);
-        //  Quaternion startRotation = players[1].transform.rotation;
-        //    Quaternion targetRotation = Quaternion.LookRotation(targetPosition - players[1].transform.position);
         mainMusic.volume = 1f;
         while (elapsedTime < 3.0f)
         {
--- TurnBasedLocalPvp/Turn Based Combat Local PVP/Assets/Scripts/SwordController.cs
+++ TurnBasedLocalPvp/Turn Based Combat Local PVP/Assets/Scripts/SwordController.cs
@@ -4,19 +4,20 @@ using UnityEngine;
 
 public class SwordController : MonoBehaviour
 {
-    public string tag;
-    private Rigidbody rb;
+    public string tag; //our tag of the gameobject we want to hit
+    private Rigidbody rb; //refrence to the rigibody
 
     private void Start()
     {
+        //get the rigibody component and freeze all constrains position and rotation
         rb = GetComponent<Rigidbody>();
         rb.constraints = RigidbodyConstraints.FreezeAll;
     }
     private void OnCollisionEnter(Collision collision)
     {
-        if (collision.collider.CompareTag(tag))
+        if (collision.collider.CompareTag(tag)) //if it collides with the tag we set up
         {
-            collision.collider.GetComponent<PlayerHealth>().LoseHealth();
+            collision.collider.GetComponent<PlayerHealth>().LoseHealth(); //we call the lose health to the object we hit
             Debug.Log("Dealt Damage " + collision.collider.name);
         }
       
